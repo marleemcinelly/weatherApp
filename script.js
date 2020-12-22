1a7ddab67bc07302d594770b7d4999c37cccc1e1
Marlee McInelly <marleemcinelly@gmail.com>
2020-12-21 17:58:46 -0700

    added js file and adjusted index

---
+++ script.js
@@ -0,0 +1,6 @@
+// OpenWeather API key fb31134226115ff2d52628fea397ed4e
+
+// api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid=fb31134226115ff2d52628fea397ed4e
+
+var userCity = "Phoenix"
+var savedCities = ["London", "Mumbai", "Tokyo"]
